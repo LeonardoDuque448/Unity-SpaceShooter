ba7522df75f53b9a9ccaf5c39f9b5292ad45ca41
Leonardo448 <leowolf17@gmail.com>
2020-06-22 02:08:17 -0500

    Movimiento de la nave y caida de meteorito

---
+++ Assets/Prefabs/PlayerCode.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCode : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(x, y, 0f) * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Prefabs/meteorito.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meteorito : MonoBehaviour
+{
+    public float speed = 1f;
+
+    private Rigidbody2D rb;
+
+    //private void Awake()
+    //{
+       
+    //}
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector2.down;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //transform.position += new Vector3(0f, -1f, 0f) * speed * Time.deltaTime;
+
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(this.gameObject);
+    }
+}
